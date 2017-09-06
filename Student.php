bab2fc140896a172abef6272a0f9eec42409cd30
ryanhhtan <ryanhhtan@gmail.com>
2017-09-05 17:29:50 -0700

    added third student record to the students array.

--- Student.php
+++ Student.php
@@ -13,39 +13,39 @@
  * @author ryan
  */
 class Student {
-	/**
-	 * Constructor of the class
-	 *
-	 */
-	function __construct() {
-		$this->surname = '';
-		$this->first_name = '';
-		$this->emails = array();
-		$this->grades = array();
-	}
+    /**
+     * Constructor of the class
+     *
+     */
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
 
-	function add_email($which, $email) {
-		$this->emails[$which] = $email;
-	}
+    function add_email($which, $email) {
+        $this->emails[$which] = $email;
+    }
 
-	function add_grade($grade) {
-		$this->grades[] = $grade;
-	}
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
 
-	function average() {
-		$total = 0;
-		foreach($this->grades as $score) {
-			$total += $score;
-		}
-		return $total / count($this->grades);
-	}
+    function average() {
+        $total = 0;
+        foreach($this->grades as $score) {
+            $total += $score;
+        }
+        return $total / count($this->grades);
+    }
 
-	function toString() {
-		$result = $this->first_name . ' ' . $this->surname;
-		$result .= ' ('.$this->average().")\n";
-		foreach($this->emails as $which=>$what)
-			$result .= $which . ': '. $what. "\n";
-		$result .= "\n";
-		return '<pre>'.$result.'</pre>';
-	}
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
 }
--- index.php
+++ index.php
@@ -12,40 +12,49 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-		// echo "I am out there ... I live";
-		
-		include('Student.php');
+        // echo "I am out there ... I live";
+        
+        include('Student.php');
 
-		$students = array();
+        $students = array();
 
-		// Add first student into the students array.
-		$first = new Student();
-		$first->surname = "Doe";
-		$first->first_name = "John";
-		$first->add_email('home','john@doe.com');
-		$first->add_email('work','jdoe@mcdonalds.com');
-		$first->add_grade(65);
-		$first->add_grade(75);
-		$first->add_grade(55);
-		$students['j123'] = $first;	
+        // Add first student into the students array.
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first; 
 
-		// Add seconde student into the students array.
-		$second = new Student();
-		$second->surname = "Einstein";
-		$second->first_name = "Albert";
-		$second->add_email('home','albert@braniacs.com');
-		$second->add_email('work1','a_einstein@bcit.ca');
-		$second->add_email('work2','albert@physics.mit.edu');
-		$second->add_grade(95);
-		$second->add_grade(80);
-		$second->add_grade(50);
-		$students['a456'] = $second;
+        // Add seconde student into the students array.
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
 
-		ksort($students);
 
-		foreach($students as $student) {
-			echo $student->toString(); 
-		}
+        // Add my info into the students array.
+        $me = new Student();
+        $me->surname = "Tan";
+        $me->first_name = "Hai Hua";
+        $me->add_email('school', 'htan45@my.bcit.ca');
+        $me->add_grade(90);
+        $students['b721'] = $me;
+
+        ksort($students);
+
+        foreach($students as $student) {
+            echo $student->toString(); 
+        }
         ?>
     </body>
 </html>
